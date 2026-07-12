e72c0c0993df2f19b0383ae2398e763f932f7dc3
codewithabhii1210 <abhisingh12102007@gmail.com>
2026-07-12 11:15:27 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,99 @@
+// var num =  42+5+572;
+// console.log(num);
+
+// var a ; // delacre kia  h 
+// let sds= 10 ; // decleare and inttialse kia h 
+
+
+// var // window meh add karta , 
+// // function scoped hota h 
+// // same name se use delaere hon hskata h 
+
+
+// // reassigment
+
+// a = 34;
+
+
+// // redeclaration
+// let a = 12;
+// let b = 34; // error
+
+
+// temprol dead zone 
+
+// utna area ki js ko  pata h ki variable ko declare karne se pehle use karne pe  error hoga
+
+console.log(s); // error
+let s= 12;
+
+// hositing impact per type 
+ // ek variable jab js meh banta h to booo doo hiso meh tut jata h ek hissa  declere part upar chala jata hai and intisilation part niche reh jata h 
+ 
+ 
+ let g =  undefined; // error nahi hoga but value undefined hoga
+ 
+ g = 12; // intisilation part 
+ 
+ let g = 12;
+
+
+
+ var --> hoist -> undefined
+ let --> hoist -> temprol dead zone (refracne error)
+ const --> hoist -> temprol dead zone (refracne error)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
